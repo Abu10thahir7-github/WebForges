4de335f8f55bef836f3f49d421c5a53a86af2b39
Abu10thahir7-github <abu10thahir7@gmail.com>
2025-04-01 20:01:45 -0700

    Refactor styles and improve layout in various components

--- src/Componets/HomePages/animation.jsx
+++ src/Componets/HomePages/animation.jsx
@@ -60,10 +60,12 @@ const Phrase = ({src}) => {
   return (
     <div className={'px-5 flex gap-5 items-center'}>
       <p className='text-[7.5vw]'> High-performing</p>
+      
       <svg width="94" height="52" viewBox="0 0 64 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M32 14H7.66L18.84 2.82L16 0L0 16L16 32L18.82 29.18L7.66 18H32V14Z" fill="#F6BC17"></path>
 <path d="M32 18L56.34 18L45.16 29.18L48 32L64 16L48 -1.39876e-06L45.18 2.82L56.34 14L32 14L32 18Z" fill="#F6BC17"></path>
 </svg>
+
     </div>
   )
 }

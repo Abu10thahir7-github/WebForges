2eeed64591a06e6ad658976035b2a05e9e35ea00
Abu10thahir7-github <abu10thahir7@gmail.com>
2025-04-14 21:03:56 -0700

    Remove HelmetProvider from main.jsx for cleaner code structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,11 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/main.scss';  // Import the Sass file
 import './index.css';
-import { HelmetProvider } from 'react-helmet-async';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HelmetProvider>
 
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  </HelmetProvider>
+
 );

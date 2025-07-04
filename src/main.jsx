c481e47a33e6259d373b1a48e667b9cfcda39636
mhd-sulu-786 <muhammedsulaimant162@gmail.com>
2025-07-05 04:40:08 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+
 
 import App from './App';
 import './styles/main.scss';  // Import the Sass file
@@ -9,8 +11,10 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <BrowserRouter>
-
+<HelmetProvider>
   <App />
+
+</HelmetProvider>
 
     <App />
   </BrowserRouter>
